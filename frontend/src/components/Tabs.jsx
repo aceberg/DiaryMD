7967a65412dad78a230cc3b6c23d16b7d9dc0439
aceberg <1502200+aceberg@users.noreply.github.com>
2024-10-08 01:43:38 +0700

    Tabs

--- frontend/src/components/Tabs.jsx
+++ frontend/src/components/Tabs.jsx
@@ -2,15 +2,23 @@ import { createResource } from "solid-js";
 import { getFileByID } from '../functions/api';
 import { currentFile, setCurrentFile, emptyFile, setCurrentTabList, currentTabList } from "../functions/exports";
 import { setEditorValue } from './Editor';
+import { Show } from "solid-js";
 
 const updTabList = (curFile) => {
   let tabs = currentTabList();
-  if ((curFile.Path !== '') && (!inArray(tabs, curFile))) {
-    tabs.push(curFile);
+
+  if (curFile.ID == 0) {
+    // console.log("Remove from Tabs", curFile);
+    tabs = removeFromArray(tabs, curFile);
     setCurrentTabList(tabs);
+  } else {
+    if (!inArray(tabs, curFile)) {
+      tabs.push(curFile);
+      setCurrentTabList(tabs);
+    }
   }
 
-  console.log("TABS", tabs);
+  // console.log("TABS", tabs);
 
   return tabs;
 };
@@ -19,38 +27,45 @@ function Tabs() {
 
   const [tabList] = createResource(currentFile, updTabList);
 
-  const handleClose = (data) => {
-    console.log("Close", data);
-    setCurrentFile(emptyFile);
-    setEditorValue('');
+  const handleClose = async () => {
+    // console.log("Close");
+
+    let file = emptyFile;
+    file.Path = currentFile().Path;
+    const tabs = await updTabList(file);
+    if (tabs.length > 0) {
+      file = tabs[0];
+    } 
+    handleFile(file);
   };
 
   const handleFile = async (file) => {
-    console.log("File", file);
-    const fileText = await getFileByID(file.ID);
+    // console.log("File", file);
+    let fileText = '';
+    if (file.ID !== 0) {
+      fileText = await getFileByID(file.ID);
+    }
     setCurrentFile(file);
     setEditorValue(fileText);
   };
 
-  // const handlePin = (tab) => {
-  //   console.log("Pin", tab);
-  //   allTabs = currentTabList();
-  //   // setCurrentTabList();
-  // };
-
   return (
     <div class='file-place'>
-      {/* <button class="tab rounded-top" title={currentFile().Path}>
-        <span class="mx-3">{currentFile().Name}</span>
-        <i class="bi bi-x tab-close" title='Close' onClick={[handleClose, '']}></i>
-      </button> */}
       <For each={[currentFile()]}>{(f) =>
         <For each={tabList()}>{(tab) =>
-        <button class="tab rounded-top" title={tab.Path}>
-          {/* <i class="bi bi-pin shade-hover" title="Pin" onClick={[handlePin, tab]}></i> */}
-          <span class="mx-3" name={f.Name} onClick={[handleFile, tab]}>{tab.Name}</span>
-          <i class="bi bi-x tab-close" title='Close' onClick={[handleClose, '']}></i>
-        </button>
+          <Show
+            when={f.Path == tab.Path}
+            fallback={
+              <button class="tab rounded-top" title={tab.Path}>
+                <span class="mx-3" onClick={[handleFile, tab]}>{tab.Name}</span>
+              </button>
+            }
+          >
+            <button class="tab-main rounded-top" title={tab.Path}>
+              <span class="mx-3">{tab.Name}</span>
+              <i class="bi bi-x tab-close" title='Close' onClick={handleClose}></i>
+            </button>
+          </Show>
         }</For>
       }</For>
     </div>
@@ -66,4 +81,15 @@ function inArray(tabs, file) {
     }
   }
   return false;
+}
+
+function removeFromArray(tabs, file) {
+  let newTabs = [];
+
+  for (let t of tabs) {
+    if (t.Path !== file.Path) {
+      newTabs.push(t);
+    }
+  }
+  return newTabs;
 }
